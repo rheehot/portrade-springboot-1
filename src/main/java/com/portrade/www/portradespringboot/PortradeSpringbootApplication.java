a7e2491688c5f10461b51f54e822a2d7d8c114b9
younggil Park <ygpark1230@gmail.com>
2021-08-03 21:05:48 +0900

    Initial commit

---
+++ src/main/java/com/portrade/www/portradespringboot/PortradeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.portrade.www.portradespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortradeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortradeSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portrade/www/portradespringboot/PortradeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portrade.www.portradespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortradeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
